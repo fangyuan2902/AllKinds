0141dabea1e1f1d5bb397a7aa388c7b8afffa780
fangyuan2902 <user@kangzefu-MBP.lan>
2017-04-28 16:30:46 +0800

    2
    2

---
+++ AllKinds/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  AllKinds
+//
+//  Created by kangzefu on 2017/4/28.
+//  Copyright © 2017年 康泽付. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
